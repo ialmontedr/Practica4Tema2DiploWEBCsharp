888441443969b5febe8da1757bae55e657419ca7
ialmontedr <55951238+ialmontedr@users.noreply.github.com>
2019-11-18 17:54:41 -0500

    Practica 4 del Capitulo 2 de Diplomado Web Con C# MVC

---
+++ ProyectoRegistroPersonasMVCCap2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoRegistroPersonasMVCCap2.Models;
+
+namespace ProyectoRegistroPersonasMVCCap2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FormularioVisitas()
+        {
+            return View();
+        }
+        public ActionResult CargaDatos()
+        {
+            string nombre = Request.Form["nombre"].ToString();
+            string cedula = Request.Form["cedula"];
+            string telefonos = Request.Form["Telefonos"];
+            string correo = Request.Form["correo"];
+            LibroVisitas libro = new LibroVisitas();
+            libro.Grabar(nombre, cedula, telefonos, correo);
+
+            return View();
+        }
+        public ActionResult ListadoVisitas()
+        {
+            LibroVisitas libro = new LibroVisitas();
+            string todo = libro.Leer();
+            ViewData["libro"] = todo;
+            return View();
+        }
+    }
+}
---
+++ ProyectoRegistroPersonasMVCCap2/Models/LibroVisitas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace ProyectoRegistroPersonasMVCCap2.Models
+{
+    public class LibroVisitas
+    {
+        public void Grabar(string nombre, string cedula, string telefonos, string correo)
+        {
+            StreamWriter archivo = new StreamWriter(HostingEnvironment.MapPath("~") + "/App_data/datos.txt", true);
+            archivo.WriteLine("Nombre:" + nombre + "<br>Cedula:" + cedula + "<br>Telefono:" + telefonos + "<br>Email:" + correo + "<hr>");
+            archivo.Close();
+        }      
+        public string Leer()
+        {
+            StreamReader archivo = new StreamReader(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt");
+            string todo = archivo.ReadToEnd();
+            archivo.Close();
+            return todo;
+        }
+    }
+}
